da514e4e464fe178a78678b946f7d0b852fff4c3
fernando3312 <32649716+fernando3312@users.noreply.github.com>
2023-10-05 18:57:49 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react-swc';
+import million from "million/compiler";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    million.vite({ auto: true }),
+    react(),
+    checker({ typescript: true, eslint: { lintCommand: 'eslint src' } }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
